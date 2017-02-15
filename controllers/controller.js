a5b74ec6b5631670de66a8e664cd9eef33b66b28
bindupothana <himabindu.pothana@navaratan.com>
2017-02-15 16:51:37 +0530

    Initial commit

---
+++ controllers/controller.js
@@ -0,0 +1,19 @@
+var app = angular.module('pyramidApp', []);
+app.controller('pyramidCtrl', function ($scope) {
+
+	var totalNumberofRows = 5;
+	$scope.pyramid= function () {
+
+    
+    for (var i = 1; i <= totalNumberofRows; i++) {
+    	
+        for (var j = 1; j <= i; j++) {
+
+            
+            console.log(j);
+        }
+        console.log("\n");
+    }
+}
+    $scope.pyramid();
+});
